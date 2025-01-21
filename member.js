15e11aca9cea5ebc9815534fe401835f450945e9
marcos-silva-cognizant <marcos.silva4@cognizant.com>
2025-01-21 19:34:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/members/skills.html',
+    controller: 'MembersController',
+    controllerAs: 'membersCtrl'
+  };
+}
